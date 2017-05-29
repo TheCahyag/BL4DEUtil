4a90e2ad3b9dc06adf7d20a4adb2ac897e506f1a
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-05-29 04:47:54 -0400

    - Added child command with skeleton

---
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/commands/Ranks/Technician.java
@@ -0,0 +1,18 @@
+package com.servegame.bl4de.BL4DEUtil.commands.Ranks;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+
+/**
+ * File: Technician.java
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class Technician implements CommandExecutor {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        return null;
+    }
+}
