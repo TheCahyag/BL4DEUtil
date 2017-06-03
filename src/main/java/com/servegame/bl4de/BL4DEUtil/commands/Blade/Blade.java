b09f34f155dab660b348d3563dad5ace78b2b937
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-06-03 15:07:03 -0400

    - Reformatting text to run commands

--- src/main/java/com/servegame/bl4de/BL4DEUtil/commands/Blade/Blade.java
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/commands/Blade/Blade.java
@@ -6,7 +6,9 @@ import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.format.TextStyles;
 
 /**
  * File: Blade.java
@@ -24,7 +26,7 @@ public class Blade implements CommandExecutor {
         Text t6 = Text.of(TextColors.GOLD, "TheCahyag\n");
         Text t7 = Text.of(TextColors.GREEN, "Commands");
         Text t8 = Text.of(TextColors.WHITE, ": /");
-        Text t9 = Text.of(TextColors.GREEN, "Blade commands\n");
+        Text t9 = Text.builder("Blade commands\n").color(TextColors.GREEN).style(TextStyles.UNDERLINE).onClick(TextActions.runCommand("/blade commands")).build();
         Text t10 = Text.of(TextColors.GRAY, "----------------------------------------------------");
         Text[] tmp = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10};
         Text message = Text.builder().append(tmp).build();
