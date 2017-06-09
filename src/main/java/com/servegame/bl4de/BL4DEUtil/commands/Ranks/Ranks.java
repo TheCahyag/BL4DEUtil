28cec003819d57f9f36430d9d0df66c3265cd138
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-06-08 22:44:16 -0400

    - Minor spacing change

--- src/main/java/com/servegame/bl4de/BL4DEUtil/commands/Ranks/Ranks.java
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/commands/Ranks/Ranks.java
@@ -55,7 +55,6 @@ public class Ranks implements CommandExecutor {
                 .onHover(TextActions.showText(Text.of("Click here to suggest this command")))
                 .build();
         Text t20 = Text.of(TextColors.GRAY, "----------------------------------------------------");
-
         Text[] tmp = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20};
         Text message = Text.builder().append(tmp).build();
         src.sendMessage(message);
