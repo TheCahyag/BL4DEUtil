4affba2a4da5097afeeaecbb2496a8164a8ec210
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-06-17 22:52:57 -0400

    - Moved the command registration actions to a separate class - Made an event listener for ChangeBlockEvent.Place

--- src/main/java/com/servegame/bl4de/BL4DEUtil/BL4DEUtil.java
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/BL4DEUtil.java
@@ -3,26 +3,19 @@
 package com.servegame.bl4de.BL4DEUtil;
 
 import com.google.inject.Inject;
-import com.servegame.bl4de.BL4DEUtil.commands.Blade.Blade;
-import com.servegame.bl4de.BL4DEUtil.commands.Blade.BladeHelp;
-import com.servegame.bl4de.BL4DEUtil.commands.GMC;
-import com.servegame.bl4de.BL4DEUtil.commands.GMS;
-import com.servegame.bl4de.BL4DEUtil.commands.Ranks.LabRat;
-import com.servegame.bl4de.BL4DEUtil.commands.Ranks.Ranks;
-import com.servegame.bl4de.BL4DEUtil.commands.Ranks.Scientist;
-import com.servegame.bl4de.BL4DEUtil.commands.Ranks.Technician;
+import com.servegame.bl4de.BL4DEUtil.eventhandlers.BL4DEEventHandler;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
-import org.spongepowered.api.command.args.GenericArguments;
-import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GameLoadCompleteEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.text.Text;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Path;
 
 /**
  * File: BL4DEUtil.java
@@ -33,11 +26,22 @@ import java.util.Map;
     url = "https://github.com/TheCahyag/BL4DEUtil")
 public class BL4DEUtil {
 
-    @Inject
-    private Game game;
+    private BL4DEEventHandler eventHandler;
+
+    @Inject private Game game;
+    @Inject private Logger logger;
 
     @Inject
-    private Logger logger;
+    @DefaultConfig(sharedRoot = true)
+    private Path defaultConfig;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private Path privateConfigDir;
 
     public Logger getLogger(){
         return this.logger;
@@ -50,73 +54,16 @@ public class BL4DEUtil {
 
     @Listener
     public void onInit(GameInitializationEvent event){
+        // Register Commands
+        BL4DECommandRegister commandRegister = new BL4DECommandRegister(this.game, this);
+        commandRegister.registerCommands();
 
-        // Register /Blade
-        // /Blade help
-        CommandSpec bladeHelp = CommandSpec.builder()
-                .description(Text.of("View commands provide with BL4DEUtil"))
-                .permission("bl4de.base")
-                .executor(new BladeHelp())
-                .build();
-        // /Blade
-        CommandSpec blade = CommandSpec.builder()
-                .description(Text.of("Information regarding the BL4DEUtil plugin"))
-                .permission("bl4de.base")
-                .child(bladeHelp, "help", "?", "commands")
-                .executor(new Blade())
-                .build();
-        this.game.getCommandManager().register(this, blade, "blade", "bl4de");
-        this.logger.info("/Blade registered");
+        // EventHandler
+        this.eventHandler = new BL4DEEventHandler(this);
+    }
 
-        // Register /Ranks
-        // /Ranks LabRat
-        CommandSpec labRat = CommandSpec.builder()
-                .description(Text.of("Show information about the LabRat rank"))
-                .permission("bl4de.ranks.base")
-                .executor(new LabRat())
-                .build();
-
-        // /Ranks Technician
-        CommandSpec technician = CommandSpec.builder()
-                .description(Text.of("Show information about the Technician rank"))
-                .permission("bl4de.ranks.base")
-                .executor(new Technician())
-                .build();
-
-        // /Ranks Scientist
-        CommandSpec scientist = CommandSpec.builder()
-                .description(Text.of("Show information about the Scientist rank"))
-                .permission("bl4de.ranks.base")
-                .executor(new Scientist())
-                .build();
-        // /Ranks
-        CommandSpec ranks = CommandSpec.builder()
-                .description(Text.of("View current ranks of the server and how they can be achieved."))
-                .permission("bl4de.ranks.base")
-                .child(labRat, "labrat")
-                .child(technician, "technician")
-                .child(scientist, "scientist")
-                .executor(new Ranks())
-                .build();
-        this.game.getCommandManager().register(this, ranks, "ranks", "rank");
-        this.logger.info("/Ranks registered");
-
-        // Register /GMC
-        CommandSpec gmc = CommandSpec.builder()
-                .description(Text.of("Sets the gamemode of the player to creative."))
-                .permission("bl4de.gamemode.creative")
-                .executor(new GMC())
-                .build();
-        this.game.getCommandManager().register(this, gmc, "gmc");
-        this.logger.info("/GMC registered");
-
-        // Register /GMS
-        CommandSpec gms = CommandSpec.builder()
-                .description(Text.of("Sets the gamemode of the player to survival."))
-                .permission("bl4de.gamemode.survival")
-                .executor(new GMS())
-                .build();
-        this.game.getCommandManager().register(this, gms, "gms");
-        this.logger.info("/GMS registered");
+    @Listener
+    public void onChangeBlockPlaceEvent(ChangeBlockEvent.Place event){
+        this.eventHandler.handleEvent(event);
     }
 }
