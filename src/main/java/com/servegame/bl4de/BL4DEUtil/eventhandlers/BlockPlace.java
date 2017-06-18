76d9493cfc5edda05a1ac93d27ba29f7631fe1e9
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-06-17 22:51:39 -0400

    - Made classes for handling events

---
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/eventhandlers/BlockPlace.java
@@ -0,0 +1,19 @@
+package com.servegame.bl4de.BL4DEUtil.eventhandlers;
+
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.block.CollideBlockEvent;
+
+import java.util.Optional;
+
+/**
+ * File: BlockPlace.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class BlockPlace {
+    public static boolean handle(ChangeBlockEvent.Place event) {
+
+        return false;
+    }
+}
