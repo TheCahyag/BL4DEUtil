ec07ce8a5891d8a0d06cae464cf0672fbc4c4a1d
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-07-01 23:50:36 -0400

    - Get a map of all players and their login times and sent that information to the command executor

---
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/commands/LastOnline.java
@@ -0,0 +1,45 @@
+package com.servegame.bl4de.BL4DEUtil.commands;
+
+import com.servegame.bl4de.BL4DEUtil.util.FileIO.FileParser;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+import java.util.*;
+
+/**
+ * File: LastOnline.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class LastOnline implements CommandExecutor {
+
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        Optional<Map<Date, String>> optionalPlayerData = FileParser.getRecentPlayerLogins();
+        if (!optionalPlayerData.isPresent()){
+            src.sendMessage(Text.of(TextColors.AQUA, "No player entries were found. (It probably couldn't access the file)"));
+            return CommandResult.empty();
+        }
+        Map<Date, String> playerData = optionalPlayerData.get();
+        ArrayList<Map.Entry<Date, String>> entries = new ArrayList<>(playerData.entrySet());
+        Text message = Text.of();
+        int size = entries.size() >= 10 ? 10 : entries.size();
+        for (int i = 0; i < size; i++) {
+            // Go threw 10 or less entries to print out as the most recently played players
+            message.toBuilder()
+                    .append(Text.of(TextColors.AQUA, entries.get(i).getValue(), " "))
+                    .append(Text.of(TextColors.GREEN, entries.get(i).getKey().toString(), "\n"))
+                    .build();
+        }
+        src.sendMessage(message);
+        return CommandResult.success();
+    }
+
+
+}
+
