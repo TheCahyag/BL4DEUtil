59a2802d5438778ca767e6baa27e903f7ea8707e
Brandon Bires-Navel <brn5915@rit.edu>
2017-07-08 13:47:49 -0400

    - Refactored message creation

--- src/main/java/com/servegame/bl4de/BL4DEUtil/commands/ranks/LabRat.java
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/commands/ranks/LabRat.java
@@ -15,24 +15,34 @@ import org.spongepowered.api.text.format.TextColors;
 public class LabRat implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        Text t1 = Text.of(TextColors.GRAY, "-----------------------");
-        Text t2 = Text.of(TextColors.WHITE, "[");
-        Text t3 = Text.of(TextColors.AQUA, "LabRat");
-        Text t4 = Text.of(TextColors.WHITE, "]");
-        Text t5 = Text.of(TextColors.GRAY, "----------------------\n");
-        Text t6 = Text.of(TextColors.AQUA, "Requirements");
-        Text t7 = Text.of(TextColors.WHITE, ": \n");
-        Text t8 = Text.of(TextColors.YELLOW, "   ➊ Log into the server\n");
-        Text t9 = Text.of(TextColors.GREEN, "Benefits");
-        Text t10 = Text.of(TextColors.WHITE, ": \n");
-        Text t11 = Text.of(TextColors.YELLOW, "   ➊ Set a home with /Sethome\n");
-        Text t12 = Text.of(TextColors.YELLOW, "   ➋ /Kit Starter\n");
-        Text t13 = Text.of(TextColors.YELLOW, "   ➌ Teleport to a random position with /RTP\n");
-        Text t14 = Text.of(TextColors.YELLOW, "   ➍ Teleport to/from players with /TPA, /TPAHere\n");
-        Text t15 = Text.of(TextColors.GRAY, "----------------------------------------------------");
-        Text[] tmp = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10,
-                t11, t12, t13, t14, t15};
-        Text message = Text.builder().append(tmp).build();
+        /*
+            -----------------------[LabRat]----------------------
+            Requirements:
+               ➊ Log into the server
+            Benefits:
+               ➊ Set a home with /Sethome
+               ➋ /Kit Starter
+               ➌ Teleport to a random position with /RTP
+               ➍ Teleport to/from players with /TPA, /TPAHere
+            ----------------------------------------------------
+         */
+        Text message = Text.builder()
+                .append(Text.of(TextColors.GRAY, "-----------------------",
+                        TextColors.WHITE, "[",
+                        TextColors.AQUA, "LabRat",
+                        TextColors.WHITE, "]",
+                        TextColors.GRAY, "----------------------\n",
+                        TextColors.AQUA, "Requirements",
+                        TextColors.WHITE, ": \n",
+                        TextColors.YELLOW, "   ➊ Log into the server",
+                        TextColors.GREEN, "Benefits",
+                        TextColors.WHITE, ": \n",
+                        TextColors.YELLOW, "   ➊ Set a home with /Sethome\n",
+                        TextColors.YELLOW, "   ➋ /Kit Starter\n",
+                        TextColors.YELLOW, "   ➌ Teleport to a random position with /RTP\n",
+                        TextColors.YELLOW, "   ➍ Teleport to/from players with /TPA, /TPAHere\n",
+                        TextColors.GRAY, "----------------------------------------------------"))
+                .build();
         src.sendMessage(message);
         return CommandResult.success();
     }
