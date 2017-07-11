6c69d42ac22fd0becc1b720b18f14bc691907906
Brandon Bires-Navel <brandonnavel@outlook.com>
2017-07-10 20:22:57 -0400

    - Created a static permission class to house all the permissions that are used in BL4DEUtil

---
+++ src/main/java/com/servegame/bl4de/BL4DEUtil/util/Permissions.java
@@ -0,0 +1,24 @@
+package com.servegame.bl4de.BL4DEUtil.util;
+
+/**
+ * File: Permissions.java
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ * The Permissions class holds static strings that are accessed to assign permissions to a given command
+ */
+public class Permissions {
+
+    // Staff commands
+    public static final String COMMAND_BLADE_DEBUG          = "bl4de.debug.base";
+    public static final String COMMAND_GMC                  = "bl4de.gamemode.creative";
+    public static final String COMMAND_GMS                  = "bl4de.gamemode.survival";
+
+    // User commands
+    public static final String COMMAND_BLADE                = "bl4de.base";
+    public static final String COMMAND_BLADE_HELP           = "bl4de.base";
+    public static final String COMMAND_GETCLW               = "bl4de.getclw.base";
+    public static final String COMMAND_GETCLW_CONFIRM       = "bl4de.getclw.confirm";
+    public static final String COMMAND_RANK                 = "bl4de.ranks.base";
+    public static final String COMMAND_LASTONLINE           = "bl4de.lastonline.base";
+    public static final String COMMAND_LASTONLINE_PLAYERm    = "bl4de.lastonline.player";
+
+}
